0a9cb5d32a6026f3410647225175ae9c51ae6a32
saeed <s.fatemi@hamisystem.ir>
2021-12-10 00:05:53 +0100

    second

---
+++ test/SimpleApi.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+using System;
+using Xunit;
+using SimpleApi.Controllers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi.Test
+{
+    public class UnitTest1
+    {
+
+        private readonly Mock<ILogger<WeatherForecastController>> _mockILogger;
+        private readonly WeatherForecastController _weatherForecastController;
+        public UnitTest1()
+        {
+            _mockILogger = new Mock<ILogger<WeatherForecastController>>();
+            _weatherForecastController = new WeatherForecastController(_mockILogger.Object);
+        }
+
+        [Fact]
+        public void Get_ShouldReturnList()
+        {
+            var returnValue = _weatherForecastController.Get(); 
+            Assert.Equal("saeed", "saeed");
+
+        }
+    }
+}
