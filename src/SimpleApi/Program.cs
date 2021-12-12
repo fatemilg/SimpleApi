75ca7d92cd9bec1ef47684f87541887a4faa2a44
saeed <s.fatemi@hamisystem.ir>
2021-12-12 12:09:49 +0100

    dd

--- src/SimpleApi/Program.cs
+++ src/SimpleApi/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
+//program
 namespace SimpleApi
 {
     public class Program
